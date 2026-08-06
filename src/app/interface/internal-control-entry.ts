5b0ab9d92ac740e160d5049cb2f22169c866c779
PQNDAAA <gayvallet.dylan@gmail.com>
2026-08-06 20:26:04 +0200

    - Internal control entry added (Interface)

---
+++ src/app/interface/internal-control-entry.ts
@@ -0,0 +1,11 @@
+export interface InternalControlEntry {
+  agentName: string;
+  domainName: string;
+  entryDate: string;
+  equipment: boolean;
+  professionalCard: boolean;
+  requiredWorkClothing: boolean;
+  workstationSetup: boolean;
+  pti: boolean;
+  comment:string;
+}
